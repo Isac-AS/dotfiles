5f1d5c65d297f377adb14962c5276fc2abdd1748
isac <isac.as@proton.me>
2024-11-10 13:24:26 +0000

    Missing autocomplete setup

--- stow_home/nvim/.config/nvim/after/plugin/lspconfig.lua
+++ stow_home/nvim/.config/nvim/after/plugin/lspconfig.lua
@@ -48,3 +48,20 @@ require('mason-lspconfig').setup({
     end,
   },
 })
+
+
+-- Autocomplete setup
+local cmp = require('cmp')
+
+cmp.setup({
+  sources = {
+    {name = 'nvim_lsp'},
+  },
+  snippet = {
+    expand = function(args)
+      -- You need Neovim v0.10 to use vim.snippet
+      vim.snippet.expand(args.body)
+    end,
+  },
+  mapping = cmp.mapping.preset.insert({}),
+})
